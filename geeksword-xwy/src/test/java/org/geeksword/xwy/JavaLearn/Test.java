3a25ae5db141ecc47218916e88ccfbcc0c5e1bda
xwy <1348773661@qq.com>
2019-04-02 23:38:18 +0800

    update

---
+++ geeksword-xwy/src/test/java/org/geeksword/xwy/JavaLearn/Test.java
@@ -0,0 +1,24 @@
+package org.geeksword.xwy.JavaLearn;
+
+/*
+ * @Author :xwy
+ * @poem：悟来时见江海古，苍崖行遍谒玄门。
+ * @Date :Created in 2019/3/12  12:45 PM
+ * @Description:
+ */
+public class Test {
+    String str = "6";
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.change(test.str);
+        System.out.println(test.str);
+        String s = "111";
+        StringBuffer stringBuffer = new StringBuffer();
+
+
+
+    }
+    public void change(String str){
+         str = "10";
+    }
+}
